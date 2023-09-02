f834c9b33d1c7272170e40ed4512ed7cff8e85e7
Mazin Salih <mazinsalih35@outlook.com>
2023-09-02 20:37:35 +0300

    Updated files

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState("Mazin Salih"));
+  const [text, setText] = useState("Mazin Salih")
+
+  const handleClick = () => {
+    if (text === "Mazin Salih") {
+     return setText("Ameera Adil")
+    } else {
+     return setText("Mazin Salih")
+    }
+  }
+  return (
+    <React.Fragment>
+      <h1> {text} </h1>
+      <button type="button" className="btn" onClick={handleClick}>change title</button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;
